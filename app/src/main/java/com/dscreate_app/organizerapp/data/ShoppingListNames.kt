c116e76ea3f3d2ff88be174693e2d38059acfe31
DmitrySlv <dscreateapp@gmail.com>
2023-09-13 15:12:06 +0400

    Создание пакетов и Entity для БД.

---
+++ app/src/main/java/com/dscreate_app/organizerapp/data/ShoppingListNames.kt
@@ -0,0 +1,29 @@
+package com.dscreate_app.organizerapp.data
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "shopping_list_names")
+@Parcelize
+data class ShoppingListNames(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
+    @ColumnInfo(name = "allItemsCounter")
+    val allItemsCounter: Int,
+
+    @ColumnInfo(name = "checkedItemsCounter")
+    val checkedItemsCounter: Int,
+
+    @ColumnInfo(name = "itemsIds")
+    val itemsIds: Int,
+): Parcelable
